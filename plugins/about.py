06621acbb0dae11a9538559ff4928980ffad51ba
Royal Dwip <120001510+Royal-Luku@users.noreply.github.com>
2023-12-22 10:38:54 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -4,10 +4,10 @@ token = os.environ.get('TOKEN','')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
-@Client.on_message(filters.private & filters.command(["about"]))
+@Client.on_message(filters.private & filters.command(["stats"]))
 async def start(client,message):
 	botdata(int(botid))
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"᚛› 𝐔𝐬𝐞𝐫𝐬 - {total_user()}\n᚛› 𝐑𝐞𝐧𝐚𝐦𝐞 𝐅𝐢𝐥𝐞𝐬 - {total_rename}\n᚛› 𝐑𝐞𝐧𝐚𝐦𝐞𝐝 𝐒𝐢𝐳𝐞 - {humanbytes(int(total_size))}",quote=True)
+	await message.reply_text(f"ʙᴏᴛ sᴛᴀᴛᴜs ʀɪɢʜᴛ ɴᴏᴡ \n\n᚛› 𝐔𝐬𝐞𝐫𝐬 - {total_user()}\n᚛› 𝐑𝐞𝐧𝐚𝐦𝐞 𝐅𝐢𝐥𝐞𝐬 - {total_rename}\n᚛› 𝐑𝐞𝐧𝐚𝐦𝐞𝐝 𝐒𝐢𝐳𝐞 - {humanbytes(int(total_size))}",quote=True)
